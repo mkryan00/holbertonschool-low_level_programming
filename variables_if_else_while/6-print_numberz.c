0ab4f4cd3026b6a3514f35cb6ad06d9a0757181a
mkryan00 <13073@holbertonstudents.com>
2026-02-10 06:05:51 +0000

    Added 6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 0 to 9
+ * Return: Always return 0 (Success)
+ */
+int main(void)
+{
+int l;
+
+for (l = '0'; l <= '9'; l++)
+
+{
+putchar(l);
+}
+
+putchar('\n');
+
+return (0);
+}
