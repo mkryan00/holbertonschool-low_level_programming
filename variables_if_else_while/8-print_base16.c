bf436032561ec27dadfbdf22ad114840245dc1c1
mkryan00 <13073@holbertonstudents.com>
2026-02-10 06:23:50 +0000

    Added 8-print_base16.

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Print hexadecimal base 16
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char l;
+
+for (l = '0'; l <= '9'; l++)
+
+{
+putchar(l);
+}
+
+for (l = 'a'; l <= 'f'; l++)
+
+{
+putchar(l);
+}
+
+putchar('\n');
+
+return (0);
+}
