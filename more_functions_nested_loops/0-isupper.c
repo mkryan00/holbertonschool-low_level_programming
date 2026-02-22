c5c373c2284ac651257d674a83cbaf5c05d6cb69
mkryan00 <13073@holbertonstudents.com>
2026-02-22 09:44:04 +0000

    0. isupper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - check the code.
+ * @c: int
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+
+}
+else
+{
+
+return (0);
+}
+
+}
