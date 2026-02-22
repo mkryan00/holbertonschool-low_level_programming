84113dec0b97321dc38b9aad3b34558372bcb34e
mkryan00 <13073@holbertonstudents.com>
2026-02-22 09:48:09 +0000

    1. isdigit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - check the code
+ * @c: int
+ * Return: Always 0.
+ */
+
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+
+}
+else
+{
+
+return (0);
+}
+}
