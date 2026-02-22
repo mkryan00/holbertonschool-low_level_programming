b94f88c15e5633207b44f829420e54580cb9272b
mkryan00 <13073@holbertonstudents.com>
2026-02-22 00:10:49 +0000

    11. 98 Battery Street, the OG

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - check the code
+ * @n: number
+ * Return: Always 0.
+ */
+
+void print_to_98(int n)
+{
+
+while (n != 98)
+{
+
+if (n < 98)
+{
+
+printf("%d", n);
+printf(", ");
+n++;
+
+}
+else
+{
+printf("%d", n);
+printf(", ");
+n--;
+
+}
+
+}
+
+printf("%d", n);
+printf("\n");
+
+}
