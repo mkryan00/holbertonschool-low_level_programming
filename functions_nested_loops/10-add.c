c5d48989174bc8421ddc00da61040bc1152b5e29
mkryan00 <13073@holbertonstudents.com>
2026-02-22 00:08:49 +0000

    10. a + b

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - check the code
+ * @x: 1 number
+ * @y: 2 number
+ * Return: Always 0.
+ */
+
+int add(int x, int y)
+{
+
+return (x + y);
+
+}
