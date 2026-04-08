9d8e39efd06a6ac3d1409c7f8ef3f028dbebda74
mkryan00 <13073@holbertonstudents.com>
2026-04-08 03:46:23 +0000

    Updated header file

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct list_s - singly linked list
@@ -24,5 +25,6 @@ typedef struct list_s
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 
 #endif
